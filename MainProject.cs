545d552530d27db1a94cbbef72d32e7fdcd49e6b
The Pyro <dooziedoodle@gmail.com>
2024-05-01 11:39:41 -0700

    Begin

---
+++ MainProject.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Formats;
+using System.Windows;
+using System.Security.Cryptography.X509Certificates;
+
+
+public class MainProject {
+    public static void Main(){
+        Windowcallprog.PrimaryDraw("1984");
+    }
+
+}
